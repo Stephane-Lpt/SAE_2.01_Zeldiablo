34be0784f4edb3f008de4fac3ae8aa672460ae84
Ratz123323 <nathan.pierrot7@etu.univ-lorraine.fr>
2023-05-30 15:03:48 +0200

    ajout de la structure des tests

---
+++ zeldiablo/project/src/test/test/TestLaby.java
@@ -0,0 +1,116 @@
+package test.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestLaby {
+
+    @Test
+    public void testGenererLaby_ok(){
+        //initialisation
+
+
+        //méthode testée
+
+
+        //vérification
+
+    }
+
+    @Test
+    public void testDeplacementPerso_haut() {
+        //initialisation
+
+
+        //méthode testée
+
+
+        //vérification
+
+    }
+
+    @Test
+    public void testDeplacementPerso_bas() {
+        //initialisation
+
+
+        //méthode testée
+
+
+        //vérification
+
+    }
+
+    @Test
+    public void testDeplacementPerso_gauche() {
+        //initialisation
+
+
+        //méthode testée
+
+
+        //vérification
+
+    }
+
+    @Test
+    public void testDeplacementPerso_droite() {
+        //initialisation
+
+
+        //méthode testée
+
+
+        //vérification
+
+    }
+
+    @Test
+    public void testDeplacementPerso_murHaut() {
+        //initialisation
+
+
+        //méthode testée
+
+
+        //vérification
+
+    }
+
+    @Test
+    public void testDeplacementPerso_murBas() {
+        //initialisation
+
+
+        //méthode testée
+
+
+        //vérification
+
+    }
+
+    @Test
+    public void testDeplacementPerso_murGauche() {
+        //initialisation
+
+
+        //méthode testée
+
+
+        //vérification
+
+    }
+
+    @Test
+    public void testDeplacementPerso_murDroit() {
+        //initialisation
+
+
+        //méthode testée
+
+
+        //vérification
+
+    }
+}
