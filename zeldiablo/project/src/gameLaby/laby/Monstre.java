761fcbbaccc612807d4ce665b06101e0b576471e
Komodzin4u <jawad.komodzinski6@etu.univ-lorraine.fr>
2023-05-31 16:41:06 +0200

    Ajout javadoc

--- zeldiablo/project/src/gameLaby/laby/Monstre.java
+++ zeldiablo/project/src/gameLaby/laby/Monstre.java
@@ -1,6 +1,16 @@
 package gameLaby.laby;
 
+/**
+ * gere un monstre situe en x,y
+ */
 public class Monstre extends Perso{
+
+    /**
+     * constructeur
+     *
+     * @param x position selon x
+     * @param y position selon y
+     */
     public Monstre(int x, int y){
         super(x, y);
     }
