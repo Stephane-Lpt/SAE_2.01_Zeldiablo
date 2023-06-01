780afb3b8a47a024c7148bb5b87e31a5d5c24396
Komodzin4u <jawad.komodzinski6@etu.univ-lorraine.fr>
2023-06-01 11:40:20 +0200

    Ajout javadoc classe héros

--- zeldiablo/project/src/gameLaby/laby/Heros.java
+++ zeldiablo/project/src/gameLaby/laby/Heros.java
@@ -1,7 +1,16 @@
 package gameLaby.laby;
 
+/**
+ * gere un héros situe en x,y
+ */
 public class Heros extends Perso{
 
+    /**
+     * constructeur
+     *
+     * @param x position selon x
+     * @param y position selon y
+     */
     public Heros(int x, int y){
         super(x, y);
     }
